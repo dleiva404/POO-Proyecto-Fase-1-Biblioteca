106621d8b023d93d5ce5bbc255d2c1a0caebe846
dleiva404 <davidleiva79@gmail.com>
2026-04-22 19:46:06 -0600

    Agregar getters, setters y toString

--- src/biblioteca/modelos/material/Material.java
+++ src/biblioteca/modelos/material/Material.java
@@ -10,9 +10,7 @@ public abstract class Material {
     private int cantidadDisponible;
     private int cantidadPrestada;
 
-    public Material(String codigoIdentificacion, String titulo,
-                    String categoria, String ubicacionFisica,
-                    int cantidadTotal) {
+    public Material(String codigoIdentificacion, String titulo, String categoria, String ubicacionFisica, int cantidadTotal) {
         this.codigoIdentificacion = codigoIdentificacion;
         this.titulo = titulo;
         this.categoria = categoria;
@@ -21,5 +19,59 @@ public abstract class Material {
         this.cantidadDisponible = cantidadTotal;
         this.cantidadPrestada = 0;
     }
+    public String getCodigoIdentificacion() {
+        return codigoIdentificacion;
+    }
+    public void setCodigoIdentificacion(String codigoIdentificacion) {
+        this.codigoIdentificacion = codigoIdentificacion;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getUbicacionFisica() {
+        return ubicacionFisica;
+    }
+    public void setUbicacionFisica(String ubicacionFisica) {
+        this.ubicacionFisica = ubicacionFisica;
+    }
+    public int getCantidadTotal() {
+        return cantidadTotal;
+    }
+    public void setCantidadTotal(int cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+    public int getCantidadPrestada() {
+        return cantidadPrestada;
+    }
+    public void setCantidadPrestada(int cantidadPrestada) {
+        this.cantidadPrestada = cantidadPrestada;
+    }
 
+    @Override
+    public String toString() {
+        return "Material{" +
+                "codigoIdentificacion='" + codigoIdentificacion + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", ubicacionFisica='" + ubicacionFisica + '\'' +
+                ", cantidadTotal=" + cantidadTotal +
+                ", cantidadDisponible=" + cantidadDisponible +
+                ", cantidadPrestada=" + cantidadPrestada +
+                '}';
+    }
 }
